5ef9a7630f4cfdb6299a1e8094e7c4b37905b652
locitakaes42 <locitakaes@gmail.com>
2025-07-16 16:16:38 +0700

    fix

--- src/main/java/com/bni/api/repository/UserRepository.java
+++ src/main/java/com/bni/api/repository/UserRepository.java
@@ -1,3 +1,4 @@
+// src/main/java/com/bni/api/repository/UserRepository.java
 package com.bni.api.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
@@ -5,10 +6,10 @@ import java.util.Optional;
 
 import com.bni.api.entity.User;
 
-// Finding existing data(s)
 public interface UserRepository extends JpaRepository<User, Long> {
     boolean existsByUsername(String username);
     boolean existsByEmail(String email);
     boolean existsByPhone(String phone);
     Optional<User> findByUsername(String username);
+    Optional<User> findByPhone(String phone); // Tambahkan ini
 }
